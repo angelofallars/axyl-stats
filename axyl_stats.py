4c1133831e2586bd2531770aff45af624b1b779b
Angelo Fallaria <ba.fallaria@gmail.com>
2021-11-17 23:10:29 +0800

    Remove code about fetching from the API in the bot code

--- axyl_stats.py
+++ axyl_stats.py
@@ -1,21 +1,19 @@
 import os
 
 import hikari
-import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 # Required .env vars
-TOKEN = os.getenv("BOT_TOKEN", None)
+BOT_TOKEN = os.getenv("BOT_TOKEN", None)
+COUNTER_CHANNEL = os.getenv("COUNTER_CHANNEL", None)
 REPO_OWNER = os.getenv("REPO_OWNER", None)
 REPO_NAME = os.getenv("REPO_NAME", None)
-COUNTER_CHANNEL = os.getenv("COUNTER_CHANNEL", None)
 
 # Optional .env vars
 INTERVAL = os.getenv("INTERVAL", 60)
-GITHUB_API_KEY = os.getenv("GITHUB_API_KEY", None)
 
-if TOKEN is None:
+if BOT_TOKEN is None:
     raise Exception("No BOT_TOKEN set in the .env file.")
 elif REPO_OWNER is None:
     raise Exception("No REPO_OWNER set in the .env file.")
@@ -23,13 +21,8 @@ elif REPO_NAME is None:
     raise Exception("No REPO_NAME set in the .env file.")
 elif COUNTER_CHANNEL is None:
     raise Exception("No COUNTER_CHANNEL set in the .env file.")
-elif GITHUB_API_KEY is None:
-    print("Warning: No GitHub API key. You will be limited to 60 requests per \
-hour.")
 
-bot = hikari.GatewayBot(token=TOKEN)
-request_link = 'https://api.github.com/repos/'\
-               + REPO_OWNER + '/' + REPO_NAME + '/releases'
+bot = hikari.GatewayBot(token=BOT_TOKEN)
 
 
 @bot.listen()
@@ -38,15 +31,8 @@ async def fetch_download_stats(event: hikari.GuildMessageCreateEvent) -> None:
         return
 
     if event.content.startswith(".stats"):
-        # TODO: Fetch the latest download info stats from the GitHub API
+        # TODO: Fetch the latest download info stats from the database
         total_download_count = 0
-
-        r = requests.get(request_link)
-        releases = r.json()
-
-        for release in releases:
-            if "assets" in release:
-                total_download_count += release["assets"][0]["download_count"]
 
         await event.message.respond(f"{REPO_NAME} has received over \
 {total_download_count} downloads!")
